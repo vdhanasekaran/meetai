c076e95f1b1401320fb0a9e44652829cce6bc009
vdhanasekaran <vivekmit06@gmail.com>
2025-06-17 22:28:01 +0800

    formatted file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,99 +6,104 @@ import Image from "next/image";
 import { useState } from "react";
 import { authClient } from "@/lib/auth-client"; 
 
+
 export default function Home() {
-
-  const { 
-        data: session
-      } = authClient.useSession();
-
+  const { data: session } = authClient.useSession();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const onSubmit = () => {
-    authClient.signUp.email({
+    authClient.signUp.email(
+      {
         email, // user email address
         password, // user password -> min 8 characters by default
         name, // user display name
-    }, {
+      },
+      {
         onRequest: (ctx) => {
-            //show loading
+          // show loading
         },
         onSuccess: (ctx) => {
-            alert("User created successfully!");
+          alert("User created successfully!");
         },
         onError: (ctx) => {
-            // display the error message
-            alert(ctx.error.message);
+          // display the error message
+          alert(ctx.error.message);
         },
-   });
-  }
+      }
+    );
+  };
 
   const onLogin = () => {
-    authClient.signIn.email({
+    authClient.signIn.email(
+      {
         email, // user email address
         password, // user password -> min 8 characters by default
-    }, {
+      },
+      {
         onRequest: (ctx) => {
-            //show loading
+          // show loading
         },
         onSuccess: (ctx) => {
-            alert("User logged in successfully!");
+          alert("User logged in successfully!");
         },
         onError: (ctx) => {
-            // display the error message
-            alert(ctx.error.message);
+          // display the error message
+          alert(ctx.error.message);
         },
-   });
-  }
+      }
+    );
+  };
 
-  if(session) {
-    return (<div className="flex flex-col p-4 gap-y-4">
-      <p>Logged in as {session.user.name}</p>
-      <Button onClick={() => authClient.signOut()}>Sign Out</Button>
-    </div>)
+  if (session) {
+    return (
+      <div className="flex flex-col p-4 gap-y-4">
+        <p>Logged in as {session.user.name}</p>
+        <Button onClick={() => authClient.signOut()}>Sign Out</Button>
+      </div>
+    );
   }
 
   return (
     <div className="flex flex-col gap-y-10">
-    <div className="p-4 flex flex-col gap-y-4">
-      <Input
-        type="text"
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <Input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button onClick={onSubmit}>Create User</Button>
-    </div>
+      <div className="p-4 flex flex-col gap-y-4">
+        <Input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={onSubmit}>Create User</Button>
+      </div>
 
-    <div className="p-4 flex flex-col gap-y-4">
-      <Input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button onClick={onLogin}>Login</Button>
+      <div className="p-4 flex flex-col gap-y-4">
+        <Input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={onLogin}>Login</Button>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
